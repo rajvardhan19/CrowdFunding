c59c54cbf48a27b75e936b2a723c1c6973e4ab03
rajvardhan19 <82277023+rajvardhan19@users.noreply.github.com>
2024-03-08 23:37:13 +0530

    created smartcontract

---
+++ contracts/CrowdFunding.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CrowdFunding {
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaign;
+
+    uint256 public numberofCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image ) {}
+
+
+    function donateToCampaign() {}
+
+
+    function getDonators() {}
+
+
+    function getCampaigns() {}
+}
